cfc3a85d73712b72251a57d9e674c1eda3998e6e
Pankaj Mishra <pankajmishra1511@gmail.com>
2021-09-10 00:47:23 +0530

    added bsc

---
+++ src/bsc.py
@@ -0,0 +1,33 @@
+import numpy as np
+import random
+
+'''
+Simulates transmission of a binary string through 
+a binary symetric channel with error probability 
+of flipping equal to p
+'''
+def BSC(inp, p = 0.5):
+    n = len(inp)
+    noise = [int(random.uniform(0, 1) < p) for _ in range(n)]
+    return [(x + y) % 2 for x, y in zip(inp, noise)]
+    
+
+
+def test_BSC():
+    print("Enter size of input: ")
+    n = int(input())
+
+    x = []
+    for i in range(n):
+        c = int(input())
+        x.append(c)
+
+    print("\nEnter BSC error probability: ")
+    p = float(input())
+
+    print("Sending the input through BSC with prob = ", p)
+
+    out = BSC(x, p)
+
+    print("Input: ", x)
+    print("Output: ", out)
